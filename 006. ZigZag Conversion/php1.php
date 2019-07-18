22977f81bcc299c4871594b1d2e0662b81b9e997
Tosone <i@tosone.cn>
2019-07-18 12:50:44 +0800

    Format question 6 php code and add question 70 solution in C++.

--- 006. ZigZag Conversion/php1.php
+++ 006. ZigZag Conversion/php1.php
@@ -1,44 +1,45 @@
 <?php
 
-class Solution {
+class Solution
+{
     /**
      * @param String $s
      * @param Integer $numRows
      * @return String
      */
-    function convert($s, $numRows) {
+    public function convert($s, $numRows)
+    {
         $res = [];
         $j = 0;
-        $flag = 1;  
+        $flag = 1;
         
-        for($i=0;$i<strlen($s);$i++){
+        for ($i=0;$i<strlen($s);$i++) {
             $res[$j][] = $s[$i];
 
-            if($j == 0 && $j == $numRows-1){ 
+            if ($j == 0 && $j == $numRows-1) {
                 $flag = -1;
             }
 
-            if(1 == $flag){
-                if($j >= $numRows -1){
+            if (1 == $flag) {
+                if ($j >= $numRows -1) {
                     $flag = 0;
                     $j--;
-                }else{
-                    $j++;        
+                } else {
+                    $j++;
                 }
-            
-            }elseif (0 == $flag){
-                if($j <= 0){
+            } elseif (0 == $flag) {
+                if ($j <= 0) {
                     $flag = 1;
                     $j++;
-                }else{
-                    $j--;   
+                } else {
+                    $j--;
                 }
-            }            
-        } 
+            }
+        }
         
         $resStr = '';
-        foreach($res as $k=>$v){
-            $resStr .= implode("",$v);
+        foreach ($res as $k=>$v) {
+            $resStr .= implode("", $v);
         }
         
         return $resStr;
