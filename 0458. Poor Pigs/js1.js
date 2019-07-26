149b25704d7ddb9dfa93201f634615edfd4a8fea
Tosone <i@tosone.cn>
2019-07-26 13:08:52 +0800

    Add solution for question 458 with java and js.

---
+++ 0458. Poor Pigs/js1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function (buckets, minutesToDie, minutesToTest) {
+    const times = minutesToTest / minutesToDie;
+    const base = times + 1;
+    // base ^ ans >= buckets 
+    // ans >= log(buckets) / log(base)
+    const temp = Math.log(buckets) / Math.log(base);
+    const ans = Math.ceil(temp)
+    return ans;
+};
+
+let buckets = 1000, minutesToDie = 15, minutesToTest = 60;
+console.log("Input: ", buckets, minutesToDie, minutesToTest);
+console.log("Output:", poorPigs(buckets, minutesToDie, minutesToTest));
