afd5aac4f49623efb07e638db4c4bda96f62242b
Tosone <i@tosone.cn>
2019-07-29 15:50:45 +0800

    Add solution for question 1 with Python.

---
+++ 0001. Two Sum/python1.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        umap = dict()
+        for i in range(0, len(nums)):
+            if umap.get(target - nums[i]) is not None:
+                return [umap.get(target - nums[i]), i]
+            umap[nums[i]] = i
+        return [-1, -1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print("Input:  {}, {}".format([1, 2, 3], 3))
+    print("Output:  {}".format(solution.twoSum([1, 2, 3], 3)))
