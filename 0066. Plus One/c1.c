de41b45d8c09bea15aa040bfe3dab70d3a7d2897
Tosone <i@tosone.cn>
2019-08-14 12:29:07 +0800

    Add solution for question 66 with c.

---
+++ 0066. Plus One/c1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int *plusOne(int *digits, int digitsSize, int *returnSize) {
+  for (int i = digitsSize - 1; i >= 0; i--) {
+    if (digits[i] == 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      break;
+    }
+  }
+
+  *returnSize = digitsSize;
+
+  int *ans;
+
+  if (digits[0] == 0) {
+    ans = (int *)malloc(sizeof(int) * (digitsSize + 1));
+    bzero(ans, sizeof(int) * (digitsSize + 1));
+    *returnSize = digitsSize + 1;
+    ans[0]      = 1;
+  } else {
+    ans = (int *)malloc(sizeof(int) * digitsSize);
+    memcpy(ans, digits, sizeof(int) * digitsSize);
+  }
+
+  return ans;
+}
+
+int main(int argc, char const *argv[]) {
+  int digits[]   = {1, 2, 3};
+  int returnSize = 0;
+
+  printf("Input:  [%d", digits[0]);
+  for (int i = 1; i < sizeof(digits) / sizeof(int); i++) {
+    printf(", %d", digits[i]);
+  }
+  printf("]\n");
+  int *ans = plusOne(digits, sizeof(digits) / sizeof(int), &returnSize);
+  printf("Output: [%d", ans[0]);
+  for (int i = 1; i < returnSize; i++) {
+    printf(", %d", ans[i]);
+  }
+  printf("]\n");
+
+  free(ans);
+
+  return EXIT_SUCCESS;
+}
