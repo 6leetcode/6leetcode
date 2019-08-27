03c14d425a29c3337ca14e12e2e98c56e40b517d
Tosone <i@tosone.cn>
2019-08-27 22:48:48 +0800

    Add custom title bar.

--- apps/miniapp/app.js
+++ apps/miniapp/app.js
@@ -1,6 +1,15 @@
 //app.js
 App({
   onLaunch: function () {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    });
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
