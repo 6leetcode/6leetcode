1b86f82dc0e8f78c67fe2195ba1931010fa3e3a8
Tosone <i@tosone.cn>
2019-11-13 12:20:15 +0800

    Add solution for question 37 in Rust.

--- Algorithm/0002. Add Two Numbers/php1.php
+++ Algorithm/0002. Add Two Numbers/php1.php
@@ -1,13 +1,16 @@
 <?php
 
-/**
- * Definition for a singly-linked list.
- * class ListNode {
- *     public $val = 0;
- *     public $next = null;
- *     function __construct($val) { $this->val = $val; }
- * }
- */
+// Definition for a singly-linked list.
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
 class Solution
 {
 
--- Algorithm/0037. Sudoku Solver/rust1.rs
+++ Algorithm/0037. Sudoku Solver/rust1.rs
@@ -1,3 +1,43 @@
+struct Solution {}
+
+const N: u8 = 3;
+const M: u8 = N * N;
+
+//   bool rule_col[N][N] = {false};
+//   bool rule_box[N][N] = {false};
+
+impl Solution {
+    pub fn solve_sudoku(_board: &mut Vec<Vec<char>>) {}
+}
+let mut rule_row = [[0u8; N], N];
+
 fn main() {
-    unimplemented!();
+    
+    let mut board: Vec<Vec<char>> = vec![
+        vec!['.', '.', '9', '7', '4', '8', '.', '.', '.'],
+        vec!['7', '.', '.', '.', '.', '.', '.', '.', '.'],
+        vec!['.', '2', '.', '1', '.', '9', '.', '.', '.'],
+        vec!['.', '.', '7', '.', '.', '.', '2', '4', '.'],
+        vec!['.', '6', '4', '.', '1', '.', '5', '9', '.'],
+        vec!['.', '9', '8', '.', '.', '.', '3', '.', '.'],
+        vec!['.', '.', '.', '8', '.', '3', '.', '2', '.'],
+        vec!['.', '.', '.', '.', '.', '.', '.', '.', '6'],
+        vec!['.', '.', '.', '2', '7', '5', '9', '.', '.'],
+    ];
+    // let origin_board: String = board.copy_from_slice(src: &[T])();
+    print!("Input:  ");
+    for row in &board {
+        for col in row {
+            print!("{}", col);
+        }
+    }
+    println!();
+    Solution::solve_sudoku(&mut board);
+    print!("Output: ");
+    for row in &board {
+        for col in row {
+            print!("{}", col);
+        }
+    }
+    println!();
 }
