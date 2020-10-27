b2740651386c176589de1b92603339052f123f38
Tosone <i@tosone.cn>
2020-10-27 22:28:45 +0800

    :sparkles: Add web vitals

--- apps/web/src/index.jsx
+++ apps/web/src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.register();
+reportWebVitals();
