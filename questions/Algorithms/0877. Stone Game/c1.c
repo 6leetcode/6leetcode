14047fb554fb841baf62a90990416ecd3e8c1191
Tosone <i@tosone.cn>
2020-11-10 21:42:00 +0800

    :bug: Fix question 877 memory leak.

--- questions/Algorithms/0877. Stone Game/c1.c
+++ questions/Algorithms/0877. Stone Game/c1.c
@@ -11,11 +11,8 @@ bool stoneGame(int *piles, int pilesSize) {
 // ------------------------------- solution end ---------------------------------
 
 int main(int argc, char const *argv[]) {
-  int input2 = 4;
-  int *input1 = (int *)malloc(sizeof(int) * input2);
-  for (int i = 0; i < input2; ++i) {
-    input1[i] = i;
-  }
+  int input1[] = {1, 2, 3, 4};
+  int input2 = (int)(sizeof(input1) / sizeof(int));
   printf("Input:  ");
   array_show(input1, input2);
   printf(" %d\n", input2);
