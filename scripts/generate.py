f0b974a207531dcc17d390b98f3b6fb0bdd2b770
Tosone <i@tosone.cn>
2021-01-17 19:44:02 +0800

    :lipstick: Format py files

--- scripts/generate.py
+++ scripts/generate.py
@@ -14,173 +14,173 @@ regex = r"(\d{4,10})\.\s([\w\W]*)"
 
 
 def rewrite_readme(name, table_content):
-  file = open(name, "r+")
-  data = file.readlines()
+    file = open(name, "r+")
+    data = file.readlines()
 
-  file.seek(0)
-  file.truncate()
+    file.seek(0)
+    file.truncate()
 
-  result = []
+    result = []
 
-  for _data in data:
-    if _data.strip() != "## Solutions' list":
-      result.append(_data.strip())
-    else:
-      result.append("## Solutions' list")
-      break
-  result.append(table_content)
+    for _data in data:
+        if _data.strip() != "## Solutions' list":
+            result.append(_data.strip())
+        else:
+            result.append("## Solutions' list")
+            break
+    result.append(table_content)
 
-  file.write("\n".join(result))
-  file.write("\n")
-  file.close()
+    file.write("\n".join(result))
+    file.write("\n")
+    file.close()
 
 
 def lang(_list, problem_name, code_prefix):
-  if len(_list) == 0:
-    return ""
-  if len(_list) == 1:
-    return "[1](" + base_code_prefix + code_prefix + problem_name.replace(" ", "%20") + "/" + _list[0] + ")"
-  else:
-    result = "[1](" + base_code_prefix + code_prefix + problem_name.replace(" ", "%20") + "/" + _list[0] + ")"
-  for i in range(1, len(_list)):
-    result += " " + "[" + str(i + 1) + "](" + base_code_prefix + code_prefix + \
-              problem_name.replace(" ", "%20") + "/" + _list[i] + ")"
-  return result
+    if len(_list) == 0:
+        return ""
+    if len(_list) == 1:
+        return "[1](" + base_code_prefix + code_prefix + problem_name.replace(" ", "%20") + "/" + _list[0] + ")"
+    else:
+        result = "[1](" + base_code_prefix + code_prefix + problem_name.replace(" ", "%20") + "/" + _list[0] + ")"
+    for i in range(1, len(_list)):
+        result += " " + "[" + str(i + 1) + "](" + base_code_prefix + code_prefix + \
+                  problem_name.replace(" ", "%20") + "/" + _list[i] + ")"
+    return result
 
 
 if __name__ == "__main__":
-  c = {}
-  cc = {}
-  go = {}
-  java = {}
-  javascript = {}
-  php = {}
-  python = {}
-  rust = {}
-  sql = {}
-  shell = {}
-  concurrency = {}
+    c = {}
+    cc = {}
+    go = {}
+    java = {}
+    javascript = {}
+    php = {}
+    python = {}
+    rust = {}
+    sql = {}
+    shell = {}
+    concurrency = {}
 
-  problems = []
+    problems = []
 
-  table = ["|Problem|C|C++|Go|Java|JS|PHP|Python|Rust|SQL|Bash|",
-           "|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"]
+    table = ["|Problem|C|C++|Go|Java|JS|PHP|Python|Rust|SQL|Bash|",
+             "|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|"]
 
-  problemsPath = []
-  for base in os.listdir(basedir):
-    for d in os.listdir(os.path.join(basedir, base)):
-      if base == "library":
-        continue
-      problemsPath.append("questions/" + base + "/" + d)
-      problems.append(d)
-      c[d] = []
-      cc[d] = []
-      go[d] = []
-      java[d] = []
-      javascript[d] = []
-      php[d] = []
-      python[d] = []
-      rust[d] = []
-      sql[d] = []
-      shell[d] = []
-      concurrency[d] = []
+    problemsPath = []
+    for base in os.listdir(basedir):
+        for d in os.listdir(os.path.join(basedir, base)):
+            if base == "library":
+                continue
+            problemsPath.append("questions/" + base + "/" + d)
+            problems.append(d)
+            c[d] = []
+            cc[d] = []
+            go[d] = []
+            java[d] = []
+            javascript[d] = []
+            php[d] = []
+            python[d] = []
+            rust[d] = []
+            sql[d] = []
+            shell[d] = []
+            concurrency[d] = []
 
-      fileList = os.listdir(os.path.join(basedir, base, d))
-      fileList.sort()
-      for inner in fileList:
-        # Golang
-        if inner.startswith("go"):
-          go[d].append(inner+"/"+inner+".go")
-        # C/C++
-        elif os.path.splitext(inner)[1] == ".c":
-          c[d].append(inner)
-        elif os.path.splitext(inner)[1] == ".cc":
-          cc[d].append(inner)
-        # PHP
-        elif os.path.splitext(inner)[1] == ".php":
-          php[d].append(inner)
-        # Rust
-        elif os.path.splitext(inner)[1] == ".rs":
-          rust[d].append(inner)
-        # Java
-        elif os.path.splitext(inner)[1] == ".java":
-          java[d].append(inner)
-        # Javascript
-        elif os.path.splitext(inner)[1] == ".js":
-          javascript[d].append(inner)
-        # Python
-        elif os.path.splitext(inner)[1] == ".py":
-          python[d].append(inner)
-        # Bash
-        elif os.path.splitext(inner)[1] == ".sh":
-          shell[d].append(inner)
-  problems.sort()
+            fileList = os.listdir(os.path.join(basedir, base, d))
+            fileList.sort()
+            for inner in fileList:
+                # Golang
+                if inner.startswith("go"):
+                    go[d].append(inner + "/" + inner + ".go")
+                # C/C++
+                elif os.path.splitext(inner)[1] == ".c":
+                    c[d].append(inner)
+                elif os.path.splitext(inner)[1] == ".cc":
+                    cc[d].append(inner)
+                # PHP
+                elif os.path.splitext(inner)[1] == ".php":
+                    php[d].append(inner)
+                # Rust
+                elif os.path.splitext(inner)[1] == ".rs":
+                    rust[d].append(inner)
+                # Java
+                elif os.path.splitext(inner)[1] == ".java":
+                    java[d].append(inner)
+                # Javascript
+                elif os.path.splitext(inner)[1] == ".js":
+                    javascript[d].append(inner)
+                # Python
+                elif os.path.splitext(inner)[1] == ".py":
+                    python[d].append(inner)
+                # Bash
+                elif os.path.splitext(inner)[1] == ".sh":
+                    shell[d].append(inner)
+    problems.sort()
 
-  for problem in problems:
-    matches = re.findall(regex, problem)
-    if len(matches) != 1:
-      continue
-    problem_id = matches[0][0]
-    problem_title = matches[0][1]
-    s = "|[" + problem_id + "](" + prefix + problem_title \
-        .replace(" ", "-").replace("(", "").replace("(", "") \
-        .replace(")", "").replace(",", "").lower() + ")"
-    if len(sql[problem]) != 0:
-      if lang(sql[problem], problem, code_prefix_database) == "":
-        continue
-      s += "|-|-|-|-|-|-|-|-|" + lang(sql[problem], problem, code_prefix_database) + "|-|"
-    if len(shell[problem]) != 0:
-      if lang(shell[problem], problem, code_prefix_shell) == "":
-        continue
-      s += "|-|-|-|-|-|-|-|-|-|" + lang(shell[problem], problem, code_prefix_shell) + "|"
-    else:
-      if lang(c[problem], problem, code_prefix_algorithm) == "" and \
-              lang(cc[problem], problem, code_prefix_algorithm) == "" and \
-              lang(go[problem], problem, code_prefix_algorithm) == "" and \
-              lang(java[problem], problem, code_prefix_algorithm) == "" and \
-              lang(javascript[problem], problem, code_prefix_algorithm) == "" and \
-              lang(php[problem], problem, code_prefix_algorithm) == "" and \
-              lang(python[problem], problem, code_prefix_algorithm) == "" and \
-              lang(rust[problem], problem, code_prefix_algorithm) == "":
-        continue
-      s += "|" + lang(c[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(cc[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(go[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(java[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(javascript[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(php[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(python[problem], problem, code_prefix_algorithm) + "|" + \
-           lang(rust[problem], problem, code_prefix_algorithm) + "|-|-|"
-    table.append(s)
+    for problem in problems:
+        matches = re.findall(regex, problem)
+        if len(matches) != 1:
+            continue
+        problem_id = matches[0][0]
+        problem_title = matches[0][1]
+        s = "|[" + problem_id + "](" + prefix + problem_title \
+            .replace(" ", "-").replace("(", "").replace("(", "") \
+            .replace(")", "").replace(",", "").lower() + ")"
+        if len(sql[problem]) != 0:
+            if lang(sql[problem], problem, code_prefix_database) == "":
+                continue
+            s += "|-|-|-|-|-|-|-|-|" + lang(sql[problem], problem, code_prefix_database) + "|-|"
+        if len(shell[problem]) != 0:
+            if lang(shell[problem], problem, code_prefix_shell) == "":
+                continue
+            s += "|-|-|-|-|-|-|-|-|-|" + lang(shell[problem], problem, code_prefix_shell) + "|"
+        else:
+            if lang(c[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(cc[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(go[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(java[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(javascript[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(php[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(python[problem], problem, code_prefix_algorithm) == "" and \
+                    lang(rust[problem], problem, code_prefix_algorithm) == "":
+                continue
+            s += "|" + lang(c[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(cc[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(go[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(java[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(javascript[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(php[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(python[problem], problem, code_prefix_algorithm) + "|" + \
+                 lang(rust[problem], problem, code_prefix_algorithm) + "|-|-|"
+        table.append(s)
 
-  rewrite_readme("README.md", "\n" + "\n".join(table))
-  rewrite_readme("README_ZH.md", "\n" + "\n".join(table))
+    rewrite_readme("README.md", "\n" + "\n".join(table))
+    rewrite_readme("README_ZH.md", "\n" + "\n".join(table))
 
-  for suffix in ["c", "cc", "rs", "go", "py", "js", "java", "php", "sh"]:
-    lang_name = suffix
-    if suffix == "py":
-      lang_name = "python"
-    elif suffix == "js":
-      lang_name = "node"
-    elif suffix == "rs":
-      lang_name = "rust"
-    elif suffix == "go":
-      lang_name = "golang"
-    elif suffix == "sh":
-      lang_name = "shell"
-    with open("testing/" + lang_name + ".makefile", "w+") as f:
-      targets = []
-      for p in problemsPath:
-        files = os.listdir(p)
-        files.sort()
-        hasLang = False
-        for _inner in files:
-          if os.path.splitext(_inner)[1] == "." + suffix:
-            hasLang = True
-          elif suffix == "go" and _inner.startswith(suffix):
-            hasLang = True
-        if hasLang:
-          targets.append("\"" + p.replace(" ", "\\ ") + "\"")
-      f.writelines("DIRS = " + " ".join(targets))
-      f.writelines("\nrun: ${DIRS}\n${DIRS}:")
-      f.writelines("\n\tmake -C $@ " + lang_name + "\n")
+    for suffix in ["c", "cc", "rs", "go", "py", "js", "java", "php", "sh"]:
+        lang_name = suffix
+        if suffix == "py":
+            lang_name = "python"
+        elif suffix == "js":
+            lang_name = "node"
+        elif suffix == "rs":
+            lang_name = "rust"
+        elif suffix == "go":
+            lang_name = "golang"
+        elif suffix == "sh":
+            lang_name = "shell"
+        with open("testing/" + lang_name + ".makefile", "w+") as f:
+            targets = []
+            for p in problemsPath:
+                files = os.listdir(p)
+                files.sort()
+                hasLang = False
+                for _inner in files:
+                    if os.path.splitext(_inner)[1] == "." + suffix:
+                        hasLang = True
+                    elif suffix == "go" and _inner.startswith(suffix):
+                        hasLang = True
+                if hasLang:
+                    targets.append("\"" + p.replace(" ", "\\ ") + "\"")
+            f.writelines("DIRS = " + " ".join(targets))
+            f.writelines("\nrun: ${DIRS}\n${DIRS}:")
+            f.writelines("\n\tmake -C $@ " + lang_name + "\n")
