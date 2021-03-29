b477de0e6ba0ab776f281fdccc1138bca062ffcb
Tosone <i@tosone.cn>
2021-03-30 01:05:57 +0800

    :bug: Fix request pallel

--- apps/cli/common/leetcode/question.go
+++ apps/cli/common/leetcode/question.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/parnurzeal/gorequest"
 	"github.com/spf13/viper"
@@ -26,10 +27,10 @@ var worker = 0
 func (i *Instance) Question(titleSlug string) (err error) {
 	if worker < maxWorker {
 		worker++
-	} else {
-		locker.Lock()
-		defer locker.Unlock()
 		defer func() { worker-- }()
+	} else {
+		<-time.After(time.Millisecond * 100)
+		return i.Question(titleSlug)
 	}
 
 	var query = fmt.Sprintf(`{"operationName":"questionData","variables":{"titleSlug":"%s"},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    langToValidPlayground\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    envInfo\n    __typename\n  }\n}\n"}`, titleSlug)
